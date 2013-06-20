50900cdcff003464a5b49f189b8dc7559cc59294
Andrew Gibiansky <agibiansky@counsyl.com>
2013-06-20 11:20:05 -0700

    Adding ipython config and aliases.

---
+++ foreign/python/ipython/profile_default/ipython_config.py
@@ -0,0 +1,31 @@
+c = get_config()  # noqa
+
+c.TerminalIPythonApp.display_banner = False
+c.InteractiveShellApp.log_level = 20
+c.InteractiveShellApp.extensions = []
+c.InteractiveShellApp.exec_lines = [
+    'import numpy',
+    'import scipy',
+    'from math import *',
+    'from __future__ import division',
+]
+c.InteractiveShellApp.exec_files = []
+c.InteractiveShell.autoindent = True
+c.InteractiveShell.colors = 'LightBG'
+c.InteractiveShell.confirm_exit = False
+c.InteractiveShell.deep_reload = True
+c.InteractiveShell.editor = 'vim'
+c.InteractiveShell.xmode = 'Context'
+
+c.PromptManager.in_template = 'In [\#]: '
+c.PromptManager.in2_template = '   .\D.: '
+c.PromptManager.out_template = 'Out[\#]: '
+c.PromptManager.justify = True
+
+c.PrefilterManager.multi_line_specials = True
+
+c.AliasManager.user_aliases = [
+    ('la', 'ls -al'),
+    ('l', 'ls'),
+    ('ll', 'ls -a'),
+]
