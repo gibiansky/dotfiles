db73378d65db444d8ea5f461302083cc382f3edb
Andrew Gibiansky <agibiansky@counsyl.com>
2013-06-27 15:52:23 -0700

    random updates

--- foreign/osx/defaults.sh
+++ foreign/osx/defaults.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -x
 
+### Set Defaults ###
+
 # Write screenshots into a Downloads subdirectory.
 defaults write com.apple.screencapture location ~/Downloads/screenshots
+
+# Show location bar in Finder.
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+
+### Restart relevant services ###
+killall Finder
+killall SystemUIServer
